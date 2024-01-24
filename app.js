a9a54d3162eec515e1d2ff54c6f27e9a463255c7
AsaadAlmughrabi <assadalmograbe8@gmail.com>
2024-01-24 20:24:56 +0000

    working on app.js and added some style

--- app.js
+++ app.js
@@ -1,48 +1,106 @@
 'use strict';
 
 
-function Employee(employeeId, fullName, department, level, imageURl, salary){
+function Employee( fullName, department, level, imageURl){
 
-    this.employeeId=employeeId;
+    this.employeeId=0;
     this.fullName=fullName;
-    this.salary=salary;
+    this.salary=1;
     this.department=department;
     this.level=level;
     this.imageURl=imageURl;
     
 }; 
 Employee.prototype.claculateSalary=function(){
-    const levelSalary = {
-        Senior: [1500, 2000],
-        "Mid-Senior": [1000, 1500],
-        Junior: [500, 1000],
-     };
+    let max;
+    let min;
+    let salary = 1;
+    if (this.level == "senior") {
+        max = 2000;
+        min = 1500;
+        salary = Math.floor(Math.random() * (max - min + 1) + min);
+        this.salary = salary - (salary * .075);
+    }
+    else if (this.level == "mid-senior") {
+        max = 1500;
+        min = 1000;
+        salary = Math.floor(Math.random() * (max - min + 1) + min);
+        this.salary = salary - (salary * .075);
+    }
+    else {
+        max = 1000;
+        min = 500;
+        salary = Math.floor(Math.random() * (max - min + 1) + min);
+        this.salary = salary - (salary * .075);
+    }
+    }
+    Employee.prototype.generanteEmpId=function(){
+        const randomId=Math.floor(Math.random() * (1024 - 1000 + 1)+1000);
+        this.employeeId=randomId;
+    }
     
-     const randomSalary = Math.floor(Math.random() * (levelSalary[this.level][1] - levelSalary[this.level][0] + 1) +levelSalary[this.level][0]);
-
-     const netSalary = randomSalary - (randomSalary * 0.075);
-    
-     this.salary = netSalary;
-    };
-
-
 Employee.prototype.renderEmployee = function(){
+    
+    const card = document.createElement('div');
+    card.className = 'card';
 
-    document.writeln(`<h3>Employee Name : <br> ${this.fullName}\n</h3><h3>Salary : ${this.salary}</h3>`);
+    const icon = document.createElement('div');
+    icon.className = 'icon';
+
+    const img = document.createElement('img');
+    img.src = this.imageURl;
+    icon.appendChild(img);
+    card.appendChild(icon);
+
+    const info = document.createElement('div');
+    info.className = 'info';
+
+    const id = document.createElement('h3');
+    id.textContent = `ID: ${this.employeeId}`;
+    info.appendChild(id);
+
+    const fullName = document.createElement('h4');
+    fullName.textContent = `Name: ${this.fullName}`;
+    info.appendChild(fullName);
+
+    const department = document.createElement('p');
+    department.textContent = `Department: ${this.department}`;
+    info.appendChild(department);
+
+    const level = document.createElement('p');
+    level.textContent = `Level: ${this.level}`;
+    info.appendChild(level);
+
+    const salary = document.createElement('p');
+    salary.textContent = `Salary: ${this.salary}`;
+    info.appendChild(salary);
+
+    card.appendChild(info);
+
+    const employeesSection = document.getElementById('employees');
+    employeesSection.appendChild(card);
 }
 
 
-let emp1 =new Employee("1000",  "Ghazi Samer","Administration","Mid-Senior","images/Ghazi.jpg",0);
-let emp2 =new Employee("1001",  "Lana Ali","Finance","Senior","images/Lana.jpg",0);
-let emp3 =new Employee("1002",  "Tamara Ayoub","Marketing","Senior","images/Tamara.jpg",0);
-let emp4 =new Employee("1003",  "Safi Walid	","Administration","Mid-Senior","images/Safi.jpg",0);
-let emp5 =new Employee("1004",  "Omar Zaid	","Development	","Senior","images/Omar.jpg",0);
-let emp6 =new Employee("1005",  "Rana Saleh	","Development","Junior","images/Rana.jpg",0);
-let emp7 =new Employee("1006",  "Hadi Ahmad	","Finance","Mid-Senior","images/Hadi.jpg",0);
-const employees = [emp1,emp2,emp3,emp4,emp5,emp6,emp7];
 
-for(let i=0;i<employees.length;i++){
-    employees[i].claculateSalary();
-    employees[i].renderEmployee();
+
+let formElement = document.getElementById("form")
+ formElement.addEventListener("submit", submitHandler);
+ formElement.addEventListener("submit", messageEvent);
+
+function submitHandler(event){
+    event.preventDefault();
+    
+    let fullName=event.target.Fname.value;
+    let department=event.target.departmment.value;
+    let level =event.target.level.value;
+    let image=event.target.img.value;
+    let newEmployee=new Employee(fullName,department,level,image);
+    this.salary=newEmployee.claculateSalary();
+    this.employeeId=newEmployee.generanteEmpId();
+    newEmployee.renderEmployee();
 }
-
+function messageEvent(event){
+    let fullName=event.target.Fname.value;
+    alert(` Hello ${fullName} You are Added Successfully`);
+}
